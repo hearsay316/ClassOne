a61de0561249d41a93c1f44176a60554f5584cb3
hearsay123 <qazwsx2228@163.com>
2019-04-16 17:46:57 +0800

    1  测试链接数据库

--- controllers/goMysql.go
+++ controllers/goMysql.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type MysqlController struct {
@@ -10,5 +13,33 @@ type MysqlController struct {
 }
 
 func (C *MysqlController) ShowMysql() {
-	sql.Open("mysql")
+	conn, err := sql.Open("mysql", "root:qq123456@tcp(sh-cdb-5pd122qv.sql.tencentcdb.com:63231)/classOne?charset=utf8")
+	if err != nil {
+		fmt.Println("连接错误", err)
+		return
+	}
+	defer conn.Close()
+	/*	_,err=conn.Exec("create table userInfo(id int,name varchar(11))")
+		if err!=nil{
+			fmt.Println("创建错误",err)
+			return
+		}*/
+
+	/* _,err=conn.Exec(`insert userInfo(id,name)VALUE (?,?)`,1,"title")
+	if err!=nil{
+		fmt.Println("创建错误",err)
+		return
+	}*/
+	rows, err := conn.Query("select name from userInfo")
+	if err != nil {
+		fmt.Println("创建错误", err)
+		return
+	}
+	var name string
+	for rows.Next() {
+		_ = rows.Scan(&name)
+		logs.Info(name, 11111111111111)
+	}
+
+	C.Ctx.WriteString("sws")
 }
--- routers/router.go
+++ routers/router.go
@@ -9,4 +9,5 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	//beego.Router("/index", &controllers.IndexController{})
 	beego.Router("/index", &controllers.IndexController{}, "get:ShowGet;post:ShowPost")
+	beego.Router("/mysql", &controllers.MysqlController{}, "get:ShowMysql")
 }
